0d3e0ebb05ae3f55ec1b4ae1a279a218bb366e43
lsy3709 <lsy3709@naver.com>
2022-12-27 17:38:44 +0900

    파이어베이스 빌드 설정 및 이메일 로그인 인증 확인 정상.

--- date_test/src/main/java/com/example/date_test/auth/JoinActivity.kt
+++ date_test/src/main/java/com/example/date_test/auth/JoinActivity.kt
@@ -5,11 +5,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import com.example.date_test.MainActivity
 import com.example.date_test.R
 import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class JoinActivity : AppCompatActivity() {
 
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -17,14 +22,35 @@ class JoinActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_join)
 
+        auth = Firebase.auth
+
         val joinBtn = findViewById<Button>(R.id.joinBtn)
         joinBtn.setOnClickListener {
 
             val email = findViewById<TextInputEditText>(R.id.emailArea)
             val pwd = findViewById<TextInputEditText>(R.id.pwdArea)
 
-            Log.d("lsy", email.text.toString())
-            Log.d("lsy", pwd.text.toString())
+//            Log.d("lsy", email.text.toString())
+//            Log.d("lsy", pwd.text.toString())
+
+            auth.createUserWithEmailAndPassword(email.text.toString(), pwd.text.toString())
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d("lsy", "createUserWithEmail:success")
+                        val user = auth.currentUser
+                        Log.d("lsy", user?.uid.toString())
+
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w("lsy", "createUserWithEmail:failure", task.exception)
+
+                    }
+                }
+
         }
 
 
